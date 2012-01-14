c92459553be5031a802a206d0b1c7f17979ae071
nagoya313 <nagoya313@gmail.com>
2012-01-14 23:13:08 +0900

    コントローラメソッドを追加

--- STNViewController.mm
+++ STNViewController.mm
@@ -1,7 +1,33 @@
 #import "STNViewController.h"
+#import "view_controller.hpp"
 
 @implementation STNViewController
 
 @synthesize controller = controller_;
+
+- (void)loadView {
+  [super loadView];
+  controller_->load_view();
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  controller_->view_will_appear(animated);
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  controller_->view_did_appear(animated);
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+  controller_->view_will_disappear(animated);
+  [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+  controller_->view_did_disappear(animated);
+  [super viewDidDisappear:animated];
+}
 
 @end
