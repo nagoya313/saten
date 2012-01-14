0fe25e0569590a9e5a3b3ef4a0f1b335409a082a
nagoya313 <nagoya313@gmail.com>
2012-01-15 02:59:39 +0900

    shared_ptrベースに変更

--- view.hpp
+++ view.hpp
@@ -1,32 +1,39 @@
 #ifndef SATEN_VIEW_HPP_
 #define SATEN_VIEW_HPP_
 #import <UIKit/UIKit.h>
+#import <boost/enable_shared_from_this.hpp>
+#import <boost/make_shared.hpp>
 #import <boost/noncopyable.hpp>
+#import <boost/shared_ptr.hpp>
 #import "color.hpp"
 #import "objective_ptr.hpp"
 
 namespace saten { namespace ui {
 template <typename View = UIView>
-class view : boost::noncopyable {
+class view : public boost::enable_shared_from_this<view<View>>, boost::noncopyable {
  public:
-  explicit view(View *v) : view_(v) {} 
+  static boost::shared_ptr<view<View>> create(View *v) {
+    return boost::make_shared<view<View>>(v);
+  }
   
-  explicit view(const saten::objective_ptr<View> &view)
-      : view_(view) {}
+  static boost::shared_ptr<view<View>> create(const CGRect &rect) {
+    return boost::make_shared<view<View>>(rect);
+  }
   
-  explicit view(const CGRect &rect)
-      : view_(make_objective([[View alloc] initWithFrame:rect])) {}
+  static boost::shared_ptr<view<View>> create(const saten::objective_ptr<View> &v) {
+    return boost::make_shared<view<View>>(v);
+  }
     
   virtual ~view() = default;
     
   template <typename View_>
-  void add_subview(const view<View_> &subview) {
-    [view_.get() addSubview:subview.get_objc_ui()];
+  void add_subview(const boost::shared_ptr<View_> &subview) {
+    [view_.get() addSubview:subview->get_objc_ui()];
   }
   
   template <typename View_>
-  void bring_subview_to_front(const view<View_> &subview) {
-    [view_.get() bringSubviewToFront:subview.get_objc_ui()];
+  void bring_subview_to_front(const boost::shared_ptr<View_> &subview) {
+    [view_.get() bringSubviewToFront:subview->get_objc_ui()];
   }
   
   void remove_from_super_view() {
@@ -72,7 +79,19 @@ class view : boost::noncopyable {
   void size_to_fit() {
     [view_.get() sizeToFit];
   }
-    
+  
+ protected:
+  explicit view(View *v) : view_(v) {}
+  
+  explicit view(const saten::objective_ptr<View> &v)
+      : view_(v) {}
+  
+  explicit view(const CGRect &rect)
+      : view_(make_objective([[View alloc] initWithFrame:rect])) {}
+  
+  template<class T, class Arg1, class... Args>
+  friend boost::shared_ptr< T > boost::make_shared( Arg1 && arg1, Args && ... args);
+  
  private:
   saten::objective_ptr<View> view_;
 };
