7275e07a6b6e08de0b3c76a7fe538519f42e2eca
nagoya313 <nagoya313@gmail.com>
2012-01-14 22:00:41 +0900

    UIViewControllerラッパを追加

---
+++ view_controller.hpp
@@ -0,0 +1,26 @@
+#ifndef SATEN_VIEW_CONTROLLER_HPP_
+#define SATEN_VIEW_CONTROLLER_HPP_
+#import <memory>
+#import <boost/noncopyable.hpp>
+#import "STNViewController.h"
+#import "objective_ptr.hpp"
+#import "view.hpp"
+
+namespace saten { namespace ui {
+class view_controller : boost::noncopyable {
+ public:
+  view_controller() 
+      : controller_(make_objective([[STNViewController alloc] init])) {
+    controller_.get().controller = this; 
+  }
+  
+  std::unique_ptr<view> get_view() const {
+    return std::unique_ptr<view>(new view(controller_.get().view));
+  }
+  
+ private:
+  saten::objective_ptr<STNViewController> controller_;
+};
+}}
+
+#endif
