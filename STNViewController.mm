9d9300cf8e434df6194818c839a749a73469970e
nagoya313 <nagoya313@gmail.com>
2012-01-14 23:33:59 +0900

    アクセス制限を回避

--- STNViewController.mm
+++ STNViewController.mm
@@ -1,32 +1,63 @@
 #import "STNViewController.h"
 #import "view_controller.hpp"
 
+namespace saten { namespace ui {
+class view_controller_accessor {
+ public:
+  explicit view_controller_accessor(view_controller *c) 
+      : controller_(c) {} 
+  
+  void load_view() {
+    controller_->load_view();
+  }
+  
+  void view_will_appear(bool animated) {
+    controller_->view_will_appear(animated);
+  }
+  
+  void view_did_appear(bool animated) {
+    controller_->view_did_appear(animated);
+  }
+  
+  void view_will_disappear(bool animated) {
+    controller_->view_will_disappear(animated);
+  }
+  
+  void view_did_disappear(bool animated) {
+    controller_->view_did_disappear(animated);
+  }
+  
+ private:
+  view_controller *controller_;
+};
+}}
+
 @implementation STNViewController
 
 @synthesize controller = controller_;
 
 - (void)loadView {
   [super loadView];
-  controller_->load_view();
+  saten::ui::view_controller_accessor(controller_).load_view();
 }
 
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
-  controller_->view_will_appear(animated);
+  saten::ui::view_controller_accessor(controller_).view_will_appear(animated);
 }
 
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
-  controller_->view_did_appear(animated);
+  saten::ui::view_controller_accessor(controller_).view_did_appear(animated);
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
-  controller_->view_will_disappear(animated);
+  saten::ui::view_controller_accessor(controller_).view_will_disappear(animated);
   [super viewWillDisappear:animated];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
-  controller_->view_did_disappear(animated);
+  saten::ui::view_controller_accessor(controller_).view_did_disappear(animated);
   [super viewDidDisappear:animated];
 }
 
